c37f6d44f013e96ad02b9865ae7ce901b353a807
David Hartsough <hartsoughdavid@gmail.com>
2021-10-26 01:36:57 -0600

    new temp banner notice

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,15 +6,8 @@ import "./global.css"
 export default ({ children }) => (
   <main>
     <div className="banner">
-      Please help improve this project by <br className="banner-break" />
-      <a
-        target="_blank"
-        rel="nofollow noopener noreferrer"
-        href="https://docs.google.com/forms/d/e/1FAIpQLSc8Tk9yx-WvhZKBFLYFst1QOIe1DASWZY3PGjiWp0Cu2GDclQ/viewform?usp=sf_link"
-      >
-        giving feedback
-      </a>{" "}
-      and <Link to="/contributing/">contributing</Link>.
+      Please note that this website is from 2018 <br className="banner-break" />
+      and will be rewritten in the coming month.
     </div>
     <header>
       <div className="top-bar">
